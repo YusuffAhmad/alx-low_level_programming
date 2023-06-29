c7c85b04f358b011a41bb0f24ab2ce11020dca62
Yusuff Ahmad <yusuffahmad2005@gmail.com>
2023-06-29 06:30:28 -0700

    strings ad pointer

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,13 +11,16 @@
  */
 void print_rev(char *s)
 {
-	int len = 0, index;
+	int length = 0;
+    int i;
 
-	while (s[index++])
-		len++;
+    /* Calculate the length of the string */
+    while (s[length] != '\0')
+        length++;
 
-	for (index = len - 1; index >= 0; index--)
-		_putchar(s[index]);
+    /* Print the string in reverse */
+    for (i = length - 1; i >= 0; i--)
+        _putchar(s[i]);
 
-	_putchar('\n');
+    _putchar('\n');
 }
